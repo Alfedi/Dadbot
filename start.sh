c6192842c9f67f3d77d86139a46f57c0d9b1aaac
Álvaro Ferrero <alfedi3867@hotmail.com>
2019-06-04 21:20:16 +0200

    Solved last commit

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export DADBOT_TOKEN=$(cat dadbot.token)
+
+echo "Getting deps..."
+mix deps.get
+
+echo "Compiling..."
+mix compile
+if [ $? != 0]; then
+    echo -e "\n\nCould not compile"
+    exit 1
+fi
+
+echo "Running..."
+mix run --no-halt
